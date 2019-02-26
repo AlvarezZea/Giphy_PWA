eaca561f117f7e22b93248c8a76a72e9d2258f46
malvar40 <malvar40@mail.depaul.edu>
2019-02-25 20:53:52 -0500

    added a title

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey = '<your giphy developer api key here>';
+const apiKey = 'your api key' ;
 const api = 'https://api.giphy.com/v1/gifs/trending'
 
 const main = document.querySelector('.container');
@@ -12,11 +12,12 @@ function createMeme(meme) {
               height='${meme.images.downsized_medium.height}'
               width='${meme.images.downsized_medium.width}'/>
         </a>
+        <p> Big Title: ${meme.title} </p> 
         </div>`
 }
 
 async function fetchTrending() {
-    const res = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${tlPpCosCdGx5ZiIGbpgmzlFtW99ps4yy}&limit=25`);
+    const res = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25`);
     const json = await res.json();
 
     main.innerHTML = json.data.map(createMeme).join('\n');
