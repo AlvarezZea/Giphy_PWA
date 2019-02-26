cc7881a9a09ef46142d15dfc0089bde7961224bc
AlvarezZea <maria_f_alvarez_zea@hotmail.com>
2019-02-25 18:15:16 -0600

    Update app.js
    insert API key

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function createMeme(meme) {
 }
 
 async function fetchTrending() {
-    const res = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25`);
+    const res = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${tlPpCosCdGx5ZiIGbpgmzlFtW99ps4yy}&limit=25`);
     const json = await res.json();
 
     main.innerHTML = json.data.map(createMeme).join('\n');
